a57214774dadbbeba0b2839e89172b7048489fd7
jiya25 <69897531+jiya25@users.noreply.github.com>
2021-05-07 15:16:51 +0530

    Add files via upload

---
+++ day 2.py
@@ -0,0 +1,7 @@
+#day 2
+
+main = True
+main =  str(main)
+
+print(main)
+print (type(main))
---
+++ day 3 age calculator.py
@@ -0,0 +1,5 @@
+#day 3 age calculator
+a=int(input("Enter your year of birth: "))
+b=2021
+age=b-a
+print(age)
